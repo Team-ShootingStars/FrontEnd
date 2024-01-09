11805abb528a67d67e422693e414fe6c2cd7796b
dnjs2721 <skadu66@gmail.com>
2024-01-09 22:12:19 +0900

    Typing Page async 수정, proxy 수정, App.js 페이지 경로 수정, 에러 페이지 추가, 로딩 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import TypingPage from "./pages/TypingPage";
 import LangSelectPage from "./pages/LangSelectPage";
 import DescriptionPage from "./pages/DescriptionPage";
 import TextListPage from "./pages/TextListPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
     return (
         <div className="App">
             <Router>
                 <Routes>
-                    <Route path={"/"} element={<LangSelectPage/>} />
-                    <Route path={":codeLang"} element={<TextListPage/>} />
-                    <Route path={":codeLang/description/:textId"} element={<DescriptionPage/>} />
+                    <Route path={"/"} element={<LangSelectPage/>}/>
+                    <Route path={":codeLang/list"} element={<TextListPage/>}/>
+                    <Route path={":codeLang/description/:textId"} element={<DescriptionPage/>}/>
                     <Route path={":codeLang/typing/:textId"} element={<TypingPage/>}/>
+                    <Route path="/NotFound" element={<NotFoundPage />} />
+                    <Route path={"*"} element={<NotFoundPage/>} />
                 </Routes>
             </Router>
         </div>
