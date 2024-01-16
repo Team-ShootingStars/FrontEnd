8d72d24b1106c5e7c6e608f323dbcbad78130e78
dnjs2721 <skadu66@gmail.com>
2024-01-16 18:51:45 +0900

    p1 modal 수정

--- src/components/modal/LangNotSelectedModal.js
+++ src/components/modal/LangNotSelectedModal.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useCallback, useEffect, useRef} from "react";
 import "../../styles/LangNotSelectedModal.css";
 
 function LangNotSelectedModal({ onClose }) {
+    const modalContentRef = useRef(null);
+
+    const handleClickOutside = useCallback((event) => {
+        if (modalContentRef.current && !modalContentRef.current.contains(event.target)) {
+            onClose();
+        }
+    }, [onClose]);
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [handleClickOutside, onClose]);
+
   return (
     <div className="p1-complete-modal">
-      <div className="p1-complete-modal-content">
-        <h2>Please select a language</h2>
+      <div ref={modalContentRef} className="p1-complete-modal-content">
+        <h2>Please select a Stadium</h2>
         <button className="p1-complete-modal-content-button" onClick={onClose}>
           Close
         </button>
