68bb4c361d4655caa6ec97b8cfdd857af704679a
tomorrowith <61576455+tomorrowith@users.noreply.github.com>
2024-01-16 10:13:21 +0900

    P3 주석처리 수정

--- src/components/DescriptionText.js
+++ src/components/DescriptionText.js
@@ -1,20 +1,76 @@
 import React from "react";
 
 function DescriptionText({longText}) {
+    const oneLine = /\/\/.*$/;
+    const multiLineStart = /\/\*.*$/;
+    const multiLineEnd = /\*\/.*$/;
+    let isMultiLineComment = false;
+
+    const styledText = longText.map((line, lineIndex) => {
+        const oneLineComment = line.match(oneLine);
+        const multiLineCommentStart = line.match(multiLineStart);
+        const multiLineCommentEnd = line.match(multiLineEnd);
+        
+        if (multiLineCommentStart) {
+            isMultiLineComment = true;
+            const notComment = line.split(multiLineCommentStart);
+            return (
+                <p>
+                    {notComment}
+                    <span className="green-text">
+                        {multiLineCommentStart}
+                    </span>
+                </p>
+            );
+        }
+
+        if (multiLineCommentEnd) {
+            isMultiLineComment = false;
+            const notComment = line.split(multiLineCommentEnd);
+            return (
+                <p>
+                    <span className="green-text">
+                        {notComment}*/
+                    </span>
+                </p>
+            );
+        }
+
+        if (oneLineComment) {
+            const notComment = line.split(oneLineComment);
+            return (
+                <p>
+                    {notComment}
+                    <span className="green-text">
+                        {oneLineComment}
+                    </span>
+                </p>
+            );
+        }
+
+        if (isMultiLineComment) {
+            return (
+                <p className="green-text">
+                    {line}
+                </p>
+            )
+        }
+
+        else {
+            return (
+                <p>
+                    {line}
+                </p>
+            );
+        }
+    });
+        
     return (
         <div className={"description-text"}>
-            {Array.from(longText).map((text, index) => (
-                <p
-                    key={index}
-                    style={{
-                    color: 'white',
-                    }}
-                >
-                    {text}
-                </p>
-            ))}   
+            {styledText}
         </div>
-    )
+    );
+
 }
 
 export default DescriptionText;
